847ac04495c810cd7aa7a329ca16162241acafa2
andres-acc <elkin.alcaraz@accenture.com>
2022-12-20 16:37:35 -0500

    Improve api configuration classification

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,24 +1,4 @@
-import { create } from 'apisauce';
-import { SnakecaseSerializer } from 'cerealizr';
-
+import api from '../config/api.sauce';
 import { IFormValues } from '../interfaces/form';
-
-const serializer = new SnakecaseSerializer();
-
-const api = create({
-  baseURL: 'https://books-training-rails.herokuapp.com/api/v1',
-  headers: {
-    contentType: 'application/json'
-  }
-});
-
-api.addRequestTransform((request) => {
-  if (request.data) {
-    request.data = serializer.serialize(request.data);
-  }
-  if (request.headers) {
-    request.headers = serializer.serialize(request.headers);
-  }
-});
 
 export const registerUser = (body: IFormValues) => api.post('/users', body);

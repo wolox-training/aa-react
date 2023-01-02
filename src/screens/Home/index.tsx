c1f638d3eb293b7a8fa9d95119117fde6f7a0ed0
andres-acc <elkin.alcaraz@accenture.com>
2023-01-02 14:25:17 -0500

    Add internationalization to the home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
+import LocalStorageService from '../../services/LocalStorageService';
 import logo from '../../assets/wolox-brand.png';
 
 import styles from './styles.module.scss';
 
 function Home() {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
+
+  const handleLogout = () => {
+    LocalStorageService.removeValue('token');
+  };
+
   return (
     <nav className={styles.navbar}>
       <img src={logo} alt="wolox" />
-      <Link className={styles.logout} to="/">
-        Logout
+      <Link className={styles.logout} onClick={handleLogout} to="/">
+        {t('Home:logout')}
       </Link>
     </nav>
   );

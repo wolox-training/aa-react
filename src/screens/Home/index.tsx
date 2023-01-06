acee27eeaf0d7e79ef9b02adbabdf17927a3f77c
andres-acc <elkin.alcaraz@accenture.com>
2023-01-06 13:52:26 -0500

    Add books list implementation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,25 +1,41 @@
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useQuery } from 'react-query';
 
 import LocalStorageService from '../../services/LocalStorageService';
+import { booksList } from '../../services/ApiService';
 import logo from '../../assets/wolox-brand.png';
+import BooksList from '../../components/App/Home/books-list';
 
 import styles from './styles.module.scss';
 
 function Home() {
   const { t } = useTranslation();
 
+  const { isLoading, isError, data } = useQuery('books', booksList);
+
   const handleLogout = () => {
     LocalStorageService.removeValue('token');
   };
 
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError || !data || !data.data) {
+    return <span>Error getting the books list</span>;
+  }
+
   return (
-    <nav className={styles.navbar}>
-      <img src={logo} alt="wolox" />
-      <Link className={styles.logout} onClick={handleLogout} to="/">
-        {t('Home:logout')}
-      </Link>
-    </nav>
+    <>
+      <nav className={styles.navbar}>
+        <img src={logo} alt="wolox" />
+        <Link className={styles.logout} onClick={handleLogout} to="/">
+          {t('Home:logout')}
+        </Link>
+      </nav>
+      <BooksList books={data.data} />
+    </>
   );
 }
 
